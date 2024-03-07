2905c7e0e5cd1ac27714d2614b3caf8549f16670
premsarja <premsagar@prodot.in>
2024-03-07 12:39:30 +0530

    done

--- main.tf
+++ main.tf
@@ -2,12 +2,17 @@ module "alb-public" {
 
   source                    = "./vendor/modules/alb"
   ENV                       = var.ENV 
+  ALB_NAME                  = roboshop-public
+  internal                  = false
 }
 
 module "alb-private" {
 
   source                    = "./vendor/modules/alb"
   ENV                       = var.ENV 
+  ALB_NAME                  = roboshop-private
+  internal                  = true
+
 }
 
 # We cannot parametrize anything that's added n the source.
